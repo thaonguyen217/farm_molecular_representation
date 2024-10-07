fd43d126c0495bfac57e8d4d7b53494367f6e0b3
thaonguyen217 <thao210798@gmail.com>
2024-10-07 01:25:06 -0500

    update download_dataset.py

--- data/download_small_corpus.py
+++ data/download_small_corpus.py
@@ -1,1 +1,8 @@
 from datasets import load_dataset
+
+# Load the dataset from the Hugging Face repository
+dataset = load_dataset("thaonguyen217/FG-enhanced-SMILES_20M")
+
+# Explore the first few examples (if the dataset is structured)
+print(f'Number of FG-enhanced SMILES in the dataset: {len(dataset)}')
+print(f'Some examples: {dataset[:10]}')
