728803783a9ee5f5958e1292907103cda008a7e4
thaonguyen217 <thao210798@gmail.com>
2024-10-07 02:08:04 -0500

    update download_data

--- data/download_small_corpus.py
+++ data/download_small_corpus.py
@@ -1,8 +1,12 @@
-from datasets import load_dataset
+from huggingface_hub import hf_hub_download
+import pickle
 
-# Load the dataset from the Hugging Face repository
-dataset = load_dataset("thaonguyen217/FG-enhanced-SMILES_20M")
+# Download the .pkl file from your Hugging Face dataset repo
+file_path = hf_hub_download(repo_id="thaonguyen217/FG-enhanced-SMILES_20M", filename="FG-enhanced-SMILES_20M.pkl")
 
-# Explore the first few examples (if the dataset is structured)
+# Load the dataset from the .pkl file
+with open(file_path, 'rb') as f:
+    dataset = pickle.load(f)
+
 print(f'Number of FG-enhanced SMILES in the dataset: {len(dataset)}')
 print(f'Some examples: {dataset[:10]}')
